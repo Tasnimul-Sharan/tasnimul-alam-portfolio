acfefe9d7fbab6a4bd427f59fbdc4d484a50d1ed
Tasnimul Alam <tasnimulsharan8962@gmail.com>
2024-04-22 00:15:13 +0600

    updated

--- src/Home/Facts.js
+++ src/Home/Facts.js
@@ -1,65 +1,132 @@
+// import React from "react";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+// import { FaStackExchange, FaRegClock } from "react-icons/fa"; // Added a new clock icon
+// import { GiProgression, GiGearHammer } from "react-icons/gi"; // Added an icon to indicate completion and technology
+// import { MdWorkOutline } from "react-icons/md"; // Added work icon
+
+// AOS.init();
+
+// const Facts = () => {
+//   return (
+    // <section className="bg-gray-900 text-white py-16">
+    //   <div className="container mx-auto">
+    //     <h1 className="text-3xl font-semibold text-info mb-8">Facts</h1>
+    //     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+    //       <div
+    //         className="bg-gradient-to-r from-blue-500 to-purple-600 rounded-lg p-6 text-center"
+    //         data-aos="fade-up"
+    //         data-aos-delay="100"
+    //         data-aos-duration="1000"
+    //       >
+    //         <GiGearHammer className="text-4xl text-primary" />
+    //         <h1 className="text-xl font-bold mt-4">20+</h1>
+    //         <p className="text-sm">Technologies Used</p>
+    //       </div>
+
+    //       <div
+    //         className="bg-gradient-to-r from-green-400 to-blue-500 rounded-lg p-6 text-center"
+    //         data-aos="fade-up"
+    //         data-aos-delay="200"
+    //         data-aos-duration="1000"
+    //       >
+    //         <GiProgression className="text-4xl text-primary" />
+    //         <h1 className="text-xl font-bold mt-4">35+</h1>
+    //         <p className="text-sm">Projects Completed</p>
+    //       </div>
+    //       <div
+    //         className="bg-gradient-to-r from-purple-500 to-pink-500 rounded-lg p-6 text-center"
+    //         data-aos="fade-up"
+    //         data-aos-delay="300"
+    //         data-aos-duration="1000"
+    //       >
+    //         <MdWorkOutline className="text-4xl text-primary" />
+    //         <h1 className="text-xl font-bold mt-4">12h+</h1>
+    //         <p className="text-sm">Daily Work Hours</p>
+    //       </div>
+    //       <div
+    //         className="bg-gradient-to-r from-red-500 to-yellow-500 rounded-lg p-6 text-center"
+    //         data-aos="fade-up"
+    //         data-aos-delay="400"
+    //         data-aos-duration="1000"
+    //       >
+    //         <FaRegClock className="text-4xl text-primary" />
+    //         <h1 className="text-xl font-bold mt-4">1</h1>
+    //         <p className="text-sm">Years of Experience</p>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
+//   );
+// };
+
+// export default Facts;
+
+
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faAtom,
-  faLeaf,
-  faT,
-  faTimesCircle,
-  faTimeline,
-} from "@fortawesome/free-solid-svg-icons";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { FaStackExchange, FaRegClock, FaCreativeCommonsShare, FaReact, FaAnchor, FaProductHunt, FaCode, FaCodepen } from "react-icons/fa"; 
+import { GiProgression, GiGearHammer } from "react-icons/gi";
+import { MdWorkOutline } from "react-icons/md"; 
 
 AOS.init();
 
 const Facts = () => {
   return (
+    
     <section className="bg-gray-900 text-white py-16">
       <div className="container mx-auto">
         <h1 className="text-3xl font-semibold text-info mb-8">Facts</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 lg:mx-10">
           <div
-            className="bg-gradient-to-r from-blue-500 to-purple-600 rounded-lg p-6 text-center"
+            className="bg-gradient-to-r from-blue-500 to-purple-600 rounded-lg p-10 flex items-center justify-center"
             data-aos="fade-up"
             data-aos-delay="100"
             data-aos-duration="1000"
           >
-            <FontAwesomeIcon icon={faLeaf} className="text-4xl text-primary" />
-            <h1 className="text-xl font-bold mt-4">19+</h1>
-            <p className="text-sm">Technologies Used</p>
+            <FaCode className="text-4xl text-primary mr-10" />
+            <div>
+              <h1 className="text-xl font-bold">20+</h1>
+              <p className="text-sm">Technologies Used</p>
+            </div>
           </div>
-
           <div
-            className="bg-gradient-to-r from-green-400 to-blue-500 rounded-lg p-6 text-center"
+            className="bg-gradient-to-r from-green-400 to-blue-500 rounded-lg p-6 flex items-center justify-center"
             data-aos="fade-up"
             data-aos-delay="200"
             data-aos-duration="1000"
           >
-            <FontAwesomeIcon icon={faAtom} className="text-4xl text-primary" />
-            <h1 className="text-xl font-bold mt-4">35+</h1>
-            <p className="text-sm">Projects Completed</p>
+            <FaReact className="text-4xl text-primary mr-10" /> 
+            <div>
+              <h1 className="text-xl font-bold">35+</h1>
+              <p className="text-sm">Projects Completed</p>
+            </div>
           </div>
-
           <div
-            className="bg-gradient-to-r from-purple-500 to-pink-500 rounded-lg p-6 text-center"
+            className="bg-gradient-to-r from-purple-500 to-pink-500 rounded-lg p-6 justify-center flex items-center"
             data-aos="fade-up"
             data-aos-delay="300"
             data-aos-duration="1000"
           >
-            <FontAwesomeIcon icon={faTimeline} className="text-4xl text-primary" />
-            <h1 className="text-xl font-bold mt-4">15h+</h1>
-            <p className="text-sm">Daily Work Hours</p>
+            <MdWorkOutline className="text-4xl text-primary mr-10" />
+            <div>
+              <h1 className="text-xl font-bold">12h+</h1>
+              <p className="text-sm">Daily Work Hours</p>
+            </div>
           </div>
 
           <div
-            className="bg-gradient-to-r from-red-500 to-yellow-500 rounded-lg p-6 text-center"
+            className="bg-gradient-to-r from-red-500 to-yellow-500 rounded-lg p-6 flex items-center justify-center"
             data-aos="fade-up"
             data-aos-delay="400"
             data-aos-duration="1000"
           >
-            <FontAwesomeIcon icon={faTimesCircle} className="text-4xl text-primary" />
-            <h1 className="text-xl font-bold mt-4">1.5+</h1>
-            <p className="text-sm">Years of Success</p>
+            <FaRegClock className="text-4xl text-primary mr-10" /> 
+            <div>
+              <h1 className="text-xl font-bold">1</h1>
+              <p className="text-sm">Years of Experience</p>
+            </div>
           </div>
         </div>
       </div>
@@ -68,5 +135,3 @@ const Facts = () => {
 };
 
 export default Facts;
-
-
